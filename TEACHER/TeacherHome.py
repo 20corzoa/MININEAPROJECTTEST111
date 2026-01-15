5319b3ffe95e347f11e252f3152b10a94467e0a9
tamoa1 <croometoma@gmail.com>
2026-01-15 23:19:08 +0000

    teacher home working. kinda just added buttons to take to other  pages

--- TEACHER/TeacherHome.py
+++ TEACHER/TeacherHome.py
@@ -1,3 +1,20 @@
 import streamlit as st
+
 st.header("Teacher Home")
-st.write(f"You are logged in as {st.session_state.role}")
+st.write(f"Logged in as {st.session_state.username} with role {st.session_state.role}")
+
+
+if st.button("Documents", use_container_width=True):
+    st.switch_page(st.Page("TEACHER/Documents.py", title="Documents"))
+
+
+if st.button("Emergency Info", use_container_width=True):
+    st.switch_page(st.Page("TEACHER/EmergencyInfo.py", title="Emergency Info"))
+
+
+if st.button("Travel Plan", use_container_width=True):
+    st.switch_page(st.Page("TEACHER/TravelPlan.py", title="Travel Plan"))
+
+
+if st.button("Register", use_container_width=True):
+    st.switch_page(st.Page("TEACHER/Register.py", title="Register"))
